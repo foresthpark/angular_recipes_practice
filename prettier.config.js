811f9bc164cc5b0223edf9dc3fcbea12a9eb6344
Forest Park <foresthpark@gmail.com>
2021-12-12 22:11:03 -0700

    changes

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// The values provided below are the defaults.
+// If you don't specify one of these properties, 
+// the default value will be applied. 
+module.exports = {
+    printWidth: 80, 
+    tabWidth: 4,    
+    useTabs: true,  
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5', // other options `es5` or `all`
+    bracketSpacing: true,
+    arrowParens: 'avoid',  // other option 'always'
+    parser: 'typescript',
+  };
